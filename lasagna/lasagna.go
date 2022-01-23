f4219f7740e515dddebfefb139debbc83898dfa9
rafanoronha <rafael.pereira@pravaler.com.br>
2022-01-23 13:49:21 -0300

    Add lasagna exercise

---
+++ lasagna/lasagna.go
@@ -0,0 +1,19 @@
+package lasagna
+
+const OvenTime int = 40
+const LayerPreparationTime int = 2
+
+// RemainingOvenTime returns the remaining minutes based on the `actual` minutes already in the oven.
+func RemainingOvenTime(actualMinutesInOven int) int {
+	return OvenTime - actualMinutesInOven
+}
+
+// PreparationTime calculates the time needed to prepare the lasagna based on the amount of layers.
+func PreparationTime(numberOfLayers int) int {
+	return numberOfLayers * LayerPreparationTime
+}
+
+// ElapsedTime calculates the total time needed to create and bake a lasagna.
+func ElapsedTime(numberOfLayers, actualMinutesInOven int) int {
+	return PreparationTime(numberOfLayers) + actualMinutesInOven
+}
